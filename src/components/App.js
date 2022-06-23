5882f95a1ec5d6012533ee96b9fc6421ba4142f0
Ben Quinn <benjamesquinn@gmail.com>
2022-06-23 15:36:37 +0100

    Added NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import '../styles/app.css';
+import React from "react";
+import "../styles/app.css";
+import NavBar from "./NavBar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Surreal Estate</h1>
+      <NavBar />
     </div>
   );
-}
+};
 
 export default App;
