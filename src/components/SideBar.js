58349e02b2185961cbcd7a7f9927c44ffa43c262
Ben Quinn <benjamesquinn@gmail.com>
2022-06-30 17:22:28 +0100

    Added sort by price functionality

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,16 +1,32 @@
 import React from "react";
 import "../styles/side-bar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import { BsFilterCircleFill } from "react-icons/bs";
+import { FaSort } from "react-icons/fa";
 
 const SideBar = () => {
+  const buildQueryString = (operation, valueObj) => {
+    const { search } = useLocation();
+
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <div className="side-bar">
       <BsFilterCircleFill className="side-bar-icon" />
       <ul className="side-bar-links">
         <li className="side-bar-links-item">
           <Link
-            to={`/?query={"city": "Manchester"}`}
+            to={buildQueryString("query", { city: "Manchester" })}
             className="item"
             data-testid="side-bar-manchester-link"
           >
@@ -18,18 +34,40 @@ const SideBar = () => {
           </Link>
         </li>
         <li className="side-bar-links-item">
-          <Link to={`/?query={"city": "Leeds"}`} className="item">
+          <Link
+            to={buildQueryString("query", { city: "Leeds" })}
+            className="item"
+          >
             Leeds
           </Link>
         </li>
         <li className="side-bar-links-item">
-          <Link to={`/?query={"city": "Liverpool"}`} className="item">
+          <Link
+            to={buildQueryString("query", { city: "Liverpool" })}
+            className="item"
+          >
             Liverpool
           </Link>
         </li>
         <li className="side-bar-links-item">
-          <Link to={`/?query={"city": "Sheffield"}`} className="item">
+          <Link
+            to={buildQueryString("query", { city: "Sheffield" })}
+            className="item"
+          >
             Sheffield
+          </Link>
+        </li>
+      </ul>
+      <FaSort className="side-bar-icon" />
+      <ul className="side-bar-links">
+        <li className="side-bar-links-item">
+          <Link to="/" className="item">
+            Price: Low to High
+          </Link>
+        </li>
+        <li className="side-bar-links-item">
+          <Link to="/" className="item">
+            Price: High to Low
           </Link>
         </li>
       </ul>
